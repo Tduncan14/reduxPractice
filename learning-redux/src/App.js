486df5df79bd2c0ee81975a908d64f9afcd8abcb
tduncan14 <nbatre1@yahoo.com>
2021-01-17 17:58:41 -0800

    keep moving forward

--- learning-redux/src/App.js
+++ learning-redux/src/App.js
@@ -1,16 +1,41 @@
 
 
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
+import {decrement, increment,islogged,loggout} from './action';
+
+
 
 function App() {
 
-  const counter = useSelector(state => state.counter)
+  const counter = useSelector(state => state.counter);
+
+  const logged = useSelector(state => state.logged)
+
+
+
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
       <header className="App-header">
            Counter {counter}
+           <button onClick ={() =>dispatch(increment())}>+</button>
+           <button onClick={() => dispatch(decrement())}>-</button>
       </header>
+
+     <button onClick={() => dispatch(islogged())}>
+       buttons
+     </button>
+
+     <button onClick={() => dispatch(loggout())}>
+       sign out
+
+     </button>
+
+
+
+
+      {logged ? <h3> Valuable information</h3> : ''}
     </div>
   );
 }
---
+++ learning-redux/src/action.js
@@ -0,0 +1,33 @@
+export const increment = () =>{
+
+    return{
+        type:'INCREMENT'
+    }
+}
+
+export const decrement = () => {
+
+
+    return{
+        type: 'DECREMENT'
+    }
+}
+
+export const islogged = () => {
+
+
+
+    return{
+        type: 'SIGN_IN'
+    }
+}
+
+
+export const loggout = () =>{
+
+
+
+    return{
+        type:'SIGN_OUT'
+    }
+}
